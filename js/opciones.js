c2f16c1c4ba1c9a7478f6490cb753473e7016606
RAlejandroZG <alejo.jordan.eco@gmail.com>
2023-11-24 13:32:14 -0500

    Bikesee V2

--- js/opciones.js
+++ js/opciones.js
@@ -1,17 +1,21 @@
 (function(){
 
+    const opciones = [...document.querySelectorAll('.knowledge__container')];
     const inicio = document.querySelector('.nav__menu');
-    const acerca_de = document.querySelector('.nav__items');
+    const acerca_de = document.querySelector('#acerca_de');
     const contacto = document.querySelector('.nav__items');
+    const waze = document.querySelector('#waze');
+    const ruta = document.querySelector('#comenzarRuta');
 
 
 
-    openButton.addEventListener('click', ()=>{
-        inicio.classList.add('nav__link--show');
+    ruta.addEventListener('click', ()=>{
+        opciones.classList.add('knowledge__container--show');
     });
 
-    closeMenu.addEventListener('click', ()=>{
-        inicio.classList.remove('nav__link--show');
+    inicio.addEventListener('click', ()=>{
+        opciones.classList.remove('nav__link--show');
     });
 
-})();
+}
+)();
